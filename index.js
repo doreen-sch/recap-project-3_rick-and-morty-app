a43a0e925398a1c83a28e796eade85afeba77ca6
Doreen Schwurack <doreen.schwurack@gmx.de>
2026-04-09 10:56:05 +0200

    fetching characters + error handling

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
+  '[data-js="search-bar-container"]',
 );
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
@@ -12,3 +12,33 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+// Fetch API
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    if (!response.ok) {
+      throw new Error(`Failed to fetch Data! Status Code: ${response.status}`);
+    }
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    return { error: error };
+  }
+}
+
+async function handleFetchCharacters() {
+  const result = await fetchCharacters();
+  if (result.error) {
+    console.log("An error occurred:", result.error);
+  }
+  return result;
+  // } else {
+  //   // console.log("Fetched data:", result.results);
+  // }
+}
+const characters = await handleFetchCharacters();
+
+console.log(characters);
